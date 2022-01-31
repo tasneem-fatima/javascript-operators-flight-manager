101a4250c65796c91c555988b551231eea987f32
Tasneem Fatima <tasneems2208@gmail.com>
2022-01-31 17:38:34 +0530

    Completed module

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,32 @@
 function Prices() {
+    const calculateFinalPrice = (basePrice, passengerTypePercent, flightTypePercent)=> {
+        let finalPrice = basePrice
+        finalPrice *= 1+passengerTypePercent/100;
+        finalPrice *= 1+flightTypePercent/100;
 
+        return finalPrice.toFixed(2);
+    }
+    const calculateDefaultFinalPrice =(basePrice, passengerType, flightType)=> {
+        let finalPrice = basePrice;
+
+        switch(passengerType.toUpperCase()){
+            case 'REGULAR': finalPrice *=0.95
+            break;
+            case 'VIP': finalPrice *=1.05
+            break;
+        }
+        switch(flightType.toUpperCase()){
+            case 'ECONOMY': finalPrice *=0.97
+            break;
+            case 'BUSINESS': finalPrice*=1.1
+            break;
+        }
+    }
+    const calculateTotalFinalPrice = (seats, basePrice, passengerType, flightType )=> {
+        return seats * calculateDefaultFinalPrice(basePrice, passengerType, flightType)
+    }
+    return { calculateFinalPrice, calculateDefaultFinalPrice, calculateTotalFinalPrice};
 }
+module.exports=Prices();
+
 
--- logic/util.js
+++ logic/util.js
@@ -4,13 +4,41 @@ function Util() {
         passengers = vipWithBusiness + vipWithEconomy + regWithBusiness + regWithEconomy
         return passengers;
     }
-const calculateTotalNumberOfPassengers=(numbersArray)=> {
-let sum = 0;
-for(i=0;i<numbersArray.length;i++){
-    sum += numbersArray[i]
-}
-}
-    return { calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
+    const calculateTotalNumberOfPassengers=(numbersArray)=> {
+    let sum = 0;
+    for(i=0;i<numbersArray.length;i++){
+        sum += numbersArray[i]
+    }
+    }
+    const checkInput = (input) => {
+        if (!input) {
+            throw new Error('Incorrect Values!')
+        }
+        if (isNaN(input)) {
+            throw new Error('Incorrect Values!')
+        }
+    }
+    const calculateTotalDistance=(distanceArray)=> {
+    let sum =0;
+    let distance;
+        for (distance of distanceArray){
+        if(distance <0){
+            continue;
+        }
+        sum += distance;
+        }
+        return sum;
+
+    }
+    const calculateBonusPoints=(businessDistArray, economyDistArray,businessBonus, economyBonus)=> {
+        let totalBusinessDistance = calculateTotalDistance(businessDistArray);
+        let totalEconomyDistance = calculateTotalDistance(economyDistArray);
+        let points = (businessBonus * totalBusinessDistance )/100 +
+            (economyBonus * totalEconomyDistance )/100
+        return points;
+
+    }
+    return { calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers, checkInput, calculateTotalDistance, calculateBonusPoints};
 }
 module.exports =Util();
 
